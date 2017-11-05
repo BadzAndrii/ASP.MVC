503e495e59a11813f5bd1cd63ef05f72712fcab3
lyubomyr wilde <lyubomyrwilde@gmail.com>
2017-11-05 21:13:34 +0200

    Add Create functionality to List / Books page

--- Lab2Server/Lab2Server/Controllers/BooksController.cs
+++ Lab2Server/Lab2Server/Controllers/BooksController.cs
@@ -1,14 +1,11 @@
 ﻿using Lab2Server.Models;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using System.Data.Entity;
 
 namespace Lab2Server.Controllers
 {
-    
+
     public class BooksController : Controller
     {
         private readonly DataContext dataContext = new DataContext();
@@ -16,11 +13,42 @@ namespace Lab2Server.Controllers
         // GET: Book
         public ActionResult List()
         {
-            dataContext.Books.Include(b => b.Sages).Load();
-            var books = dataContext.Books.ToList(); // All books with authors
-            
-            var model = books.Select(x => new BookModel { Name = x.Name, Id = x.Id, Year = x.Year, Authors = string.Join(", " , x.Sages.Select(y=> y.Name)), Description = x.Description });         
+            var model = new ListBookModels();
+
+            dataContext.Books.Include(b => b.Sages).Load(); // All books with authors
+
+            var books = dataContext
+                .Books
+                .OrderBy(x => x.Name)
+                .ToList(); //Load All books in Memory
+
+            var authors = dataContext
+                .Sages
+                .Select(x => new { Id = x.Id, Name = x.Name })
+                .OrderBy(x => x.Name)
+                .ToDictionary(key => key.Id, val => val.Name);
+
+            foreach (var item in authors)
+                model.ListAuthors.Add(item.Key, item.Value);
+
+            model.Books.AddRange( books.Select(x => new BookModel { Name = x.Name, Id = x.Id, Year = x.Year, Authors = string.Join(", " , x.Sages.Select(y=> y.Name)), Description = x.Description }) );
+
             return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Create(ListBookModels model)
+        {
+            var authors = dataContext
+                    .Sages.Where(author => model.Authors.Contains(author.Id))
+                    .ToList();
+
+            var newBook = new Book { Name = model.Name, Year = model.Year, Description = model.Description, Sages = authors };
+
+            dataContext.Books.Add(newBook);
+            dataContext.SaveChanges();
+
+            return Redirect("List");
         }
     }
 }
