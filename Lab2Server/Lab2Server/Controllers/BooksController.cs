6c78f2db6c012c31587cc363f79e712d804c88bd
lyubomyr wilde <lyubomyrwilde@gmail.com>
2017-11-05 17:51:03 +0200

    cerate home controller and its actions

---
+++ Lab2Server/Lab2Server/Controllers/BooksController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab2Server.Controllers
+{
+    public class BooksController : Controller
+    {
+        // GET: Books
+        public ActionResult List()
+        {
+            return View();
+        }
+    }
+}
---
+++ Lab2Server/Lab2Server/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace Lab2Server.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly Dictionary<string, string> _menu = new Dictionary<string, string>
+        {
+            { "Login", "/login/index"},
+            { "Books", "/books/list" },
+            { "Authors", "/authors/list" },
+            { "About", "/about/details" },
+        };
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View(_menu);
+        }
+
+        //[HttpPost]
+        //public ActionResult Detalis()
+        //{
+        //    return View("Index", _menu);
+        //}
+    }
+}
