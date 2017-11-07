bba557a95363449eec09989897806746ab880a0a
Andrii.Badz <andriy20221@gmail.com>
2017-11-07 11:26:05 +0200

    Add SageListModel , Add functional to Create new Sage

---
+++ Lab2Server/Lab2Server/Controllers/AuthorsController.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using Lab2Server.Models;
+using System.Data.Entity;
+
+namespace Lab2Server.Controllers
+{
+    public class AuthorsController : Controller
+    {
+        private readonly DataContext dataContext = new DataContext();
+
+        // GET: Authors
+        public ActionResult List()
+        {
+            var model = new ListSageModels();
+
+
+            var sages = dataContext
+                .Sages
+                .OrderBy(s => s.Name)
+                .ToList(); //Load ALL Sages in Memory
+
+
+            model.Sages.AddRange(sages.Select(s => new SageModel { Name = s.Name, Id = s.Id, Age = s.Age, City = s.City }));
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Create(ListSageModels model)
+        {
+            var newSage = new Sage { Name = model.Name, Age = model.Age, City = model.City };
+
+            dataContext.Sages.Add(newSage);
+            dataContext.SaveChanges();
+            return Redirect("List");
+        }
+    }
+}
---
+++ Lab2Server/Lab2Server/Models/ListSageModels.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab2Server.Models
+{
+    public class ListSageModels : SageModel
+    {
+        public ListSageModels ()
+        {
+            Sages = new List<SageModel>(0);
+        }
+        // PRE- !!!VIEW!!!
+        public List<SageModel> Sages { get; private set; }
+    }
+}
