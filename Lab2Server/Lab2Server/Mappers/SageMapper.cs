32e0acba0c6b69c63f3cc5b60d02331651dc1be5
Andrii.Badz <andriy20221@gmail.com>
2017-12-04 18:35:29 +0200

    Badz first challenge with Repository

---
+++ Lab2Server/Lab2Server/Mappers/SageMapper.cs
@@ -0,0 +1,38 @@
+﻿using Lab2Server.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab2Server.Mappers
+{
+    public static class SageMapper
+    {
+        public static Sage MapFromModel(this SageModel model, Sage sage)
+        {
+            sage.Name = model.Name;
+            sage.Age = model.Age;
+            sage.City = model.City;
+
+            return sage;
+        }
+
+        public static SageModel MapToSageModel(this Sage sage)
+        {
+            return new SageModel
+            {
+                Id = sage.Id,
+                Name = sage.Name,
+                City = sage.City,
+            };
+        }
+        public static ListSageModels MapTolistSageModel(this List<Sage> sages, Dictionary<int, string> authorsDictionary)
+        {
+            var model = new ListSageModels();
+
+            model.Sages.AddRange(sages.Select(x => new SageModel { Name = x.Name, Id = x.Id, City = x.City, Age = x.Age }));
+
+            return model;
+        }
+    }
+}
---
+++ Lab2Server/Lab2Server/Repositories/SagesRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab2Server.Repositories
+{
+    public class SagesRepository
+    {
+    }
+}
