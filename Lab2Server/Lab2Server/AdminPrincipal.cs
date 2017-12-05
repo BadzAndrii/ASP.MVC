a6611d10236af35945b949f64d20c8092ace4c6d
lyubomyr-wilde <lyubomyrwilde@gmail.com>
2017-12-06 01:02:00 +0200

    small clean-up

--- Lab2Server/Lab2Server/AdminPrincipal.cs
+++ Lab2Server/Lab2Server/AdminPrincipal.cs
@@ -1,14 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Principal;
-using System.Web;
+﻿using System.Security.Principal;
 
 namespace Lab2Server
 {
     public class AdminPrincipal : IPrincipal
     {
         public IIdentity Identity { get; private set; }
+
         public AdminPrincipal ( string email )
         {
             Identity = new GenericIdentity(email);
@@ -18,7 +15,5 @@ namespace Lab2Server
         {
             return true;
         }
-
-
     }
 }
